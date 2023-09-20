a18207f816e02c4cae4337262a6aab1878c2179a
unknown <elmaximolider75@gmail.com>
2023-09-20 18:11:43 -0500

    modificaciones finales

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,9 @@ import SideBar from "./components/SideBar.vue";
 <template>
   <div class="flex md:items-center md:justify-center h-screen bg-slate-100">
     <div
-      class="flex md:bg-white md:w-11/12 md:h-5/6 lg:w-10/12 lg:h-4/6 xl:w-7/12 xl:h-5/6 2xl:w-6/12 2xl:h-4/6 rounded-2xl md:p-3"
+      class="flex md:bg-white md:w-11/12 lg:w-10/12 lg:h-4/6 xl:w-7/12 xl:h-5/6 2xl:w-6/12 2xl:h-4/6 rounded-2xl md:p-3"
     >
-      <div
-        class="h-min md:h-auto w-full md:w-auto bg-yellow-200 absolute md:relative z-10"
-      >
+      <div class="h-min md:h-auto w-full md:w-auto absolute md:relative z-10">
         <Side-Bar class="hidden md:block" />
         <Navbar class="md:hidden" />
       </div>
