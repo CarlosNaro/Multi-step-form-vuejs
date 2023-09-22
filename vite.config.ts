9fcacf6bbc793f1bb81f4d798c903e73a12ce2ea
CarlosNaro <elmaximolider75@gmail.com>
2023-09-22 12:16:58 -0500

    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://CarlosNaro.github.io/Multi-step-form-vuejs/",
+  base: "https://CarlosNaro.github.io/",
 });
